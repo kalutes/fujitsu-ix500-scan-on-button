210eb895fc390a41a737afd529687808d42f2861
Kameron Lutes <kameo315@gmail.com>
2026-01-11 22:19:15 +0900

    Use -A mode for pdfcropmargins

--- scan.sh
+++ scan.sh
@@ -2,4 +2,4 @@
 now=`date +"%Y-%m-%d-%H%M%S"`
 outfilename="scan-$now.pdf"
 /app/sane-scan-pdf/scan -d -r 300 -v --mode 'Color' --page-height 999 --page-width 216 --no-default-size -o /tmp/$outfilename
-pdfcropmargins -v -s -u -p 0 /tmp/$outfilename -o /scans/$outfilename
+pdfcropmargins -v -s -u -A -p 0 /tmp/$outfilename -o /scans/$outfilename
